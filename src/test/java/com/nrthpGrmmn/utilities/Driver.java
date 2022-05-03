ee664f8ed2789139eefafb4faba3e0bdff1158e1
BLSoftUser <bluelaser.soft@gmail.com>
2022-05-03 19:00:26 -0400

    Creating Framework

--- src/test/java/com/nrthpGrmmn/utilities/Driver.java
+++ src/test/java/com/nrthpGrmmn/utilities/Driver.java
@@ -1,14 +1,55 @@
 package com.nrthpGrmmn.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
 
-    private static WebDriver obj;
+    /** 1. create obj for manipulations inside methods */
+    private static WebDriver driver;
 
-    private  Driver() { }
+    /** 2. Create Driver() */
+    private Driver() {
+    }
 
+    /** 3. Create getDriver() in case if Driver was null */
+    public static WebDriver getDriver() {
+        //3.1. create obj-browserName using ConfigReader//read(String key)
+        String browserName = ConfigReader.read("browser");
 
+        //3.2. conditions for the driver:
+        if(driver == null){
+            switch (browserName){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox" :
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+
+                default:
+                    driver = null;
+                    System.out.println("We are not working with this type of browser");
+            }
+        }else {
+            return driver;
+        }
+
+        //3.3.
+        return driver;
+    }
+
+    /** 4. Closing driver */
+    public static void closeBrowser(){
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
 
 
 }
