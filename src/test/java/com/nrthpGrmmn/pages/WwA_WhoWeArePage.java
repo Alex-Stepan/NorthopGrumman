c0e3a72da644dfaf05ecc5ef9521044b9446f8ed
BLSoftUser <bluelaser.soft@gmail.com>
2022-05-04 13:36:04 -0400

    Creating Pages classes

---
+++ src/test/java/com/nrthpGrmmn/pages/WwA_WhoWeArePage.java
@@ -0,0 +1,41 @@
+package com.nrthpGrmmn.pages;
+
+import com.nrthpGrmmn.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class WwA_WhoWeArePage {
+
+    //------------------------------------------------------//
+
+    /** 1. Use initialization of the page: */
+    public WwA_WhoWeArePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    //------------------------------------------------------//
+
+
+    /** 2. WebElements: */
+
+    /*
+    @FindBy(xpath = "//button[normalize-space()='Save and Close']")
+    public WebElement saveAndCloseButton;
+    // used for US_04
+
+    @FindBy(name = "oro_contact_form[firstName]")
+    public WebElement firstName;
+    // used for US_04
+*/
+
+
+    //------------------------------------------------------//
+
+
+    /** 3. Methods: */
+
+
+
+    //------------------------------------------------------//
+
+
+}
